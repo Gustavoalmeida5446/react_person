fde0d73437c625e4754f25b0f3e79347a43d9ef5
gustavo <gustavoalmeida5446@gmail.com>
2026-03-09 19:06:49 -0300

    requested changes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
   let partnerText = 'I am not married';
 
   if (isMarried) {
-    const title = sex === 'm' ? 'wife' : 'husband';
+    const title = sex === 'm'
+      ? 'wife'
+      : 'husband';
 
     partnerText = `${partnerName} is my ${title}`;
   }
@@ -14,10 +22,14 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
+      
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
+      )}
 
-      {age && <p className="Person__age">{`I am ${age}`}</p>}
-
-      <p className="Person__partner">{partnerText}</p>
+      <p className="Person__partner">
+        {partnerText}
+      </p>
     </section>
   );
 };
